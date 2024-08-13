fe04b90d1633176d564174a4490bda0410909bf9
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-08-13 16:37:31 +0500

    created

---
+++ 01_basics/03_convertOperation.js
@@ -0,0 +1,25 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let number = 33
+
+let string = String(number)
+console.log(string);
+console.log(typeof string);
+
