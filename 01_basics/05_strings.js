ca07a981151cfade7fb58ccd4b779d243a38b664
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-08-15 16:36:00 +0500

    updated

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+const name = 'hassan'
+const repo = 50
+
+// console.log(name + repo + 'value');
+
+// console.log(`Hello my name is ${name} and my repo is ${repo}`);
+
+const game = new String("hassan-ha")
+
+// console.log(game[0]);
+
+// console.log(game.length);
+// console.log(game.toUpperCase());
+// console.log(game.charAt(5));
+// console.log(game.indexOf('s'));
+
+// const string = game.substring(0 , 4 )
+// console.log(string);
+
+
+// let another = game.slice(-8 ,4 )
+// console.log(another);
+
+
+// const strOne = "  hassan  "
+// console.log(strOne);
+// console.log(strOne.trim());
+
+
+let url = 'https://www.facebook.com/login/'
+
+console.log(url.replace('facebook' , 'fb'));
+
+
+console.log(url.includes('insta'));
+
+
+
+console.log(game.split('a'));
+
