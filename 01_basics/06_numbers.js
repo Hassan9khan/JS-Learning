55dca3165f859df3fe84f04b3f01dd1c9df1268a
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-08-22 15:58:30 +0500

    number updated

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,39 @@
+let score1= 200
+let score = new Number(100)
+
+// console.log(score , score1);
+
+const balance = 700.98534
+
+// console.log(balance.toString());
+
+// console.log(balance.toFixed(0));
+
+const goals = 233.9873
+
+// console.log(typeof(goals.toPrecision(4)));
+
+const hundred = 1000000
+
+// console.log(hundred.toLocaleString('ur-PK'));
+
+
+// const check = Number
+ 
+// console.log(Number.MIN_VALUE);
+
+
+let maths = 25
+// console.log(Math.round(maths));
+// console.log(Math.ceil(maths));
+
+let any = Math.floor(Math.random() * 6) + 1
+// console.log(any);
+
+
+let min = 10 
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
